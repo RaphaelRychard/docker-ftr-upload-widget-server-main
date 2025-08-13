1e489a5856da595a27cad0e62f0a79ea73b2f535
Raphael Rychard <raph.rych@gmail.com>
2025-08-13 09:22:33 -0400

    add

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from "fastify";
+import { log } from "../infra/logger";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
+    log.info('Health check request received');
     await reply.status(200).send({ message: 'OK!' })
   })
 }
